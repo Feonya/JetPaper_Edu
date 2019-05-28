85b0efdef8e88734a1c43505e499923c318c3193
feonya <feonya@outlook.com>
2019-05-28 17:47:27 +0800

    7.2 移动

---
+++ Assets/Scripts/Charactors/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Transform playerTransform;
+
+    public float speed;
+
+    private void Start()
+    {
+        playerTransform = transform;
+    }
+
+    private void FixedUpdate()
+    {
+        KeyboardMove();
+    }
+
+    private void KeyboardMove()
+    {
+        Vector3 kv = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+
+        playerTransform.position += kv * speed;
+    }
+}
