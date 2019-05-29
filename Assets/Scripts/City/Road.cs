d187e92ac4862779cc0cc358a8fb9602fafbdb25
feonya <feonya@outlook.com>
2019-05-29 16:28:33 +0800

    7.3 碰撞

---
+++ Assets/Scripts/City/Road.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class Road : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("（1）主角与路面碰撞！");
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/RoadBlock.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class RoadBlock : MonoBehaviour
+{
+    private GameObject player;
+
+    private void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject == player)
+        {
+            Debug.Log("（2）主角与路障碰撞！");
+        }
+    }
+}
